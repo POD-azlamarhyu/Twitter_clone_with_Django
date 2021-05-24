33c0767ca2185357aae8ed6a3e43903dab170654
jikobouei-capital <adrague.ggoch@gmail.com>
2021-05-25 03:05:58 +0900

    add login form

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,3 +11,11 @@ class UsrCreateForm(UserCreationForm):
     def __init__(self,*args,**kwargs):
         kwargs.setdefault('label_suffix','')
         super().__init__(*args,**kwargs)
+
+class LoginForm(AuthenticationForm):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs['class']='form-control'
+            field.widget.attrs['placeholder'] = field.label
