bd2d78361ffd9050dd3d1b066f55ab23cd090b47
jikobouei-capital <adrague.ggoch@gmail.com>
2021-06-06 17:34:17 +0900

    add retweet

--- tweet/serializers.py
+++ tweet/serializers.py
@@ -8,14 +8,15 @@ TWEET_ACTION_OPTIONS = settings.TWEET_ACTION_OPTIONS
 class TweetActionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     action = serializers.CharField()
-
+    text = serializers.CharField(allow_blank=True,required=False)
+    print(text)
     def validate_action(self, value):
         value = value.lower().strip() # "Like " -> "like"
         if not value in TWEET_ACTION_OPTIONS:
             raise serializers.ValidationError("This is not a valid action for tweets")
         return value
 
-class TweetSerializer(serializers.ModelSerializer):
+class TweetCreateSerializer(serializers.ModelSerializer):
     like = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Tweet
@@ -23,7 +24,25 @@ class TweetSerializer(serializers.ModelSerializer):
     def get_like(self, obj):
         return obj.like.count()
     
-    def validate_content(self, value):
+    def validate_text(self, value):
         if len(value) > MAX_TWEET_LENGTH:
             raise serializers.ValidationError("This tweet is too long")
         return value
+class TweetSerializer(serializers.ModelSerializer):
+    like = serializers.SerializerMethodField(read_only=True)
+    retweet = TweetCreateSerializer(read_only=True)
+    class Meta:
+        model = Tweet
+        fields = ['id', 'text', 'like','images','videos','is_retweet',"retweet"]
+        
+    def get_like(self, obj):
+        return obj.like.count()
+    
+    # def get_text(self,obj):
+    #     text = obj.text
+    #     if obj.is_retweet:
+    #         text = obj.retweet.text
+    #     return obj.text
+    
+
+
